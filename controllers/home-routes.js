98a1b68062ff17d424d18bb57e4d2949c006dd43
Ozanne Alexander <alexanoz@outlook.com>
2023-02-28 17:12:04 -0500

    added the handlebars for the single post, comments, and home page and all blog posts; adjusted some controllers

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,14 +5,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     res.render('homepage', {
-        // render is used to specify which handlebars template we want to be directed to
-        id: 1,
-        post_url: 'https://handlebarsjs.com/guide/', title: 'Handlebars Docs',
-        created_at: new Date(),
-        vote_count: 10,
-        comments: [{}, {}], user: {
-            username: 'test_user'
-        }
+    
     });
 });
 
